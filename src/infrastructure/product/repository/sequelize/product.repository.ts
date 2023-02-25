d93e678f9507ea7bd66372803aacf6b09caee1b1
Lucas Machado <lucas.machado@ewally.com.br>
2023-02-25 20:21:41 -0300

    feat: find product use case and tests implemented

--- src/infrastructure/product/repository/sequelize/product.repository.ts
+++ src/infrastructure/product/repository/sequelize/product.repository.ts
@@ -27,6 +27,7 @@ export default class ProductRepository implements ProductRepositoryInterface {
 
   async find(id: string): Promise<Product> {
     const productModel = await ProductModel.findOne({ where: { id } });
+    if (!productModel) throw Error("Product not found");
     return new Product(productModel.id, productModel.name, productModel.price);
   }
 
